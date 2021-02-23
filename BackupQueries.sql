1fa2f49e8c10684e8a48fac5b58fc6c45803f86b
Iago Grah <iagobg02@gmail.com>
2021-02-23 15:38:54 -0300

    Backup Fix

--- BackupQueries.sql
+++ BackupQueries.sql
@@ -1,5 +1,7 @@
+USE master;
 BACKUP DATABASE Teach TO DISK = '/var/opt/mssql/data/Teach.bak';
 
+USE master;
 RESTORE DATABASE Teach FROM DISK = '/var/opt/mssql/data/Teach.bak' WITH REPLACE;
 
 DROP DATABASE Teach;
